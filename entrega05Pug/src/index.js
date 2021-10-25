38d7bb93400ae98533c0f1ef6b0d5236291dda67
atxpaul <pablo.pan.veira@gmail.com>
2021-10-25 17:44:14 +0200

    Preparando entrega Pug

---
+++ entrega05Pug/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const routerProductos = require('./router/routerProductos');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('form.pug', {});
+});
+app.get('/productos', async (req, res) => {
+  const productos = await axios.get('http://localhost:8080/api/productos');
+  res.render('products.pug', { productos: productos.data });
+});
+
+app.use('/api/productos', routerProductos);
+
+const PORT = 8080;
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+});
+server.on('error', (error) => console.log(`Error en servidor ${error}`));
