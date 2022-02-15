656509bc34f9544078a6c5f8976775aac934d94c
atxpaul <pablo.pan.veira@gmail.com>
2022-02-15 23:27:32 +0100

    Se desacopla normalizr

---
+++ entrega19Capas/src/middleware/normalizr.js
@@ -0,0 +1,21 @@
+// Arrancamos normalizr
+import normalizr from 'normalizr';
+
+const normalize = normalizr.normalize;
+const schema = normalizr.schema;
+
+// Definimos esquemas
+const user = new schema.Entity('author');
+
+const message = new schema.Entity('messages', {
+  author: user,
+});
+
+const messageArray = [message];
+
+async function normalization(dataArray) {
+  const normalizedData = normalize(dataArray, messageArray);
+  return normalizedData;
+}
+
+export default normalization;
